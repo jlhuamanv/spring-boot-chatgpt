e85866c0a40336e3201914b9a62380bacf158822
arq.oidt <usuarq@arq.oidt.pc>
2023-09-02 18:51:34 -0500

    Primer commit a la server de fuentes

---
+++ src/test/java/pe/org/jhsystem/demos/chatgpt/SpringBootChatgptApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.org.jhsystem.demos.chatgpt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootChatgptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
